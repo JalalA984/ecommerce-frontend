6793a477dc7b0ac20ce50c6dcff2fe2f2d3d13d1
Jalal <128756237+JalalA984@users.noreply.github.com>
2025-01-23 17:12:23 -0500

    implement router outlet, todo adapt SPA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './home/home.component';
+
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./home/home.component').then((m) => m.HomeComponent);
+    },
+  },
+
+  {
+    path: 'login',
+    loadComponent: () => {
+      return import('./login/login.component').then(
+        (module) => module.LoginComponent
+      );
+    },
+  },
+
+  {
+    path: 'add-product',
+    loadComponent: () => {
+      return import('./add-product/add-product.component').then(
+        (module) => module.AddProductComponent
+      );
+    },
+  },
+];
