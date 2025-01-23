554511e547d2e5063ba5359c06228a67e9aa72bd
Jalal <128756237+JalalA984@users.noreply.github.com>
2025-01-22 19:21:46 -0500

    setup homepage and api request

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-    {
-        path: "",
-        pathMatch: "full",
-        loadComponent: () => {
-            return import("./home/home.component").then((m) => m.HomeComponent);
-        },
-
-    },
-];
+export const routes: Routes = [];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Product } from '../models/product.type';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  serverIsUp = signal(false);
 
+  productService = inject(ProductsService);
+
+  products = signal<Array<Product>>([]);
+
+  ngOnInit(): void {
+    // Subscribing to the product service to fetch data
+    this.productService.getProductsFromBackend().subscribe({
+      next: (prods) => {
+        this.products.set(prods); // Update the product signal
+        this.serverIsUp.set(true); // Mark server as up
+      },
+      error: (err) => {
+        console.error('Failed to fetch products:', err); // Log errors for debugging
+        this.serverIsUp.set(false); // Ensure server status reflects failure
+      },
+    });
+  }
 }
+
